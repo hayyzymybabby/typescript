63e4f6f5f5abfd8246b144e4037b8fee61a8e37b
usamihal <usamihal001@163.com>
2021-10-25 17:30:51 +0800

    登陆功能开发

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
-import express, { Request, Response, NextFunction } from 'express'
+import express from 'express'
+import cookieSession from 'cookie-session'
 import router from './router'
 
 const app = express()
 app.use(express.urlencoded({ extended: false }))
-app.use((req: Request, res: Response, next: NextFunction) => {
-  req.teacherName = 'dell'
-  next()
-})
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['teacher dell'],
+    maxAge: 24 * 60 * 60 * 1000
+  })
+)
 app.use(router)
 
 app.listen(7001, () => {
