f09f02a074338098fe8ac1be31ec5fe66d61d39e
usamihal <usamihal001@163.com>
2022-10-19 23:02:16 +0800

    init

--- src/decorator/use.ts
+++
@@ -1,8 +0,0 @@
-import { RequestHandler } from 'express'
-import { CrowllerController, LoginController } from '../controller'
-
-export function use(middleware: RequestHandler) {
-  return function (target: CrowllerController | LoginController, key: string) {
-    Reflect.defineMetadata('middleware', middleware, target, key)
-  }
-}
