0c494930c02f0d6cb1b4e7e203bb171039b30b8a
usamihal <usamihal001@163.com>
2023-04-24 10:24:41 +0800

    feat: 使用cheerio进行数据提取

--- src/crowller.ts
+++ src/crowller.ts
@@ -1,13 +1,39 @@
 import superagent from 'superagent'
+import { load } from 'cheerio'
+
+interface Course {
+  title: string
+  count: number
+}
 
 class Crowller {
   private secret = 'x3b174jsx'
   private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`
-  private rawHtml = ''
+
+  getCourseInfo(html: string) {
+    const $ = load(html)
+    const courseInfos: Course[] = []
+    const courseItems = $('.course-item')
+    courseItems.map((index, element) => {
+      const descs = $(element).find('.course-desc')
+      const title = descs.eq(0).text()
+      const count = parseInt(descs.eq(1).text().split('：')[1])
+      courseInfos.push({
+        title,
+        count
+      })
+    })
+    const result = {
+      time: new Date().getTime(),
+      data: courseInfos
+    }
+
+    console.log(result)
+  }
 
   async getRawHtml() {
     const result = await superagent.get(this.url)
-    this.rawHtml = result.text
+    this.getCourseInfo(result.text)
   }
 
   constructor() {
