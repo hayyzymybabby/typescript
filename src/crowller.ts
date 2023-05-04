6fa0ad6eba7a9ffb5f8a23fa7357c47f48e07fef
usamihal <usamihal001@163.com>
2023-05-04 12:19:02 +0800

    style: 使用组合设计模式优化代码

--- src/crowller.ts
+++ src/crowller.ts
@@ -1,71 +1,38 @@
 import fs from 'fs'
 import path from 'path'
 import superagent from 'superagent'
-import { load } from 'cheerio'
+import DellAnalyzer from './dellAnalyzer'
 
-interface Course {
-  title: string
-  count: number
-}
-
-interface CourseResult {
-  time: number
-  data: Course[]
-}
-
-interface Content {
-  [propName: number]: Course[]
+export interface Analyzer {
+  analyze: (html: string, filePath: string) => string
 }
 
 class Crowller {
-  private secret = 'x3b174jsx'
-  private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`
-
-  getCourseInfo(html: string) {
-    const $ = load(html)
-    const courseInfos: Course[] = []
-    const courseItems = $('.course-item')
-    courseItems.map((index, element) => {
-      const descs = $(element).find('.course-desc')
-      const title = descs.eq(0).text()
-      const count = parseInt(descs.eq(1).text().split('：')[1])
-      courseInfos.push({
-        title,
-        count
-      })
-    })
-    return {
-      time: new Date().getTime(),
-      data: courseInfos
-    }
-  }
+  private filePath = path.resolve(__dirname, '../data/course.json')
 
   async getRawHtml() {
     const result = await superagent.get(this.url)
     return result.text
   }
 
-  generateJsonContent(courseInfo: CourseResult) {
-    const filePath = path.resolve(__dirname, '../data/course.json')
-    let fileContent: Content = {}
-    if (fs.existsSync(filePath)) {
-      fileContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
-    }
-    fileContent[courseInfo.time] = courseInfo.data
-    return fileContent
+  writeFile(content: string) {
+    fs.writeFileSync(this.filePath, content)
   }
 
   async initSpiderProcess() {
     const html = await this.getRawHtml()
-    const courseInfo = this.getCourseInfo(html)
-    const fileContent = this.generateJsonContent(courseInfo)
-    const filePath = path.resolve(__dirname, '../data/course.json')
-    fs.writeFileSync(filePath, JSON.stringify(fileContent))
+    const fileContent = this.analyzer.analyze(html, this.filePath)
+    this.writeFile(fileContent)
   }
 
-  constructor() {
+  constructor(private url: string, private analyzer: Analyzer) {
     this.initSpiderProcess()
   }
 }
 
-const crowller = new Crowller()
+const secret = 'x3b174jsx'
+const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`
+
+const analyzer = new DellAnalyzer()
+
+new Crowller(url, analyzer)
