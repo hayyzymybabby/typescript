6fed5f982f62fbbd11b041c0a224130ad4ee65f1
usamihal <usamihal001@163.com>
2023-05-11 12:27:29 +0800

    style: 解决使用TS编写常规express代码遇到的问题

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 import router from './router'
 
 const app = express()
-
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(router)
 
 app.listen(7001, () => {
--- src/router.ts
+++ src/router.ts
@@ -5,15 +5,29 @@ import DellAnalyzer from './dellAnalyzer'
 const router = Router()
 
 router.get('/', (req: Request, res: Response) => {
-  res.send('hello world')
+  res.send(`
+    <html>
+      <body>
+        <form method="post" action="/getData">
+          <input type="password" name="password" />
+          <button>提交</button>
+        </form>
+      </body>
+    </html>
+  `)
 })
 
-router.get('/getData', (req: Request, res: Response) => {
-  const secret = 'x3b174jsx'
-  const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`
-  const analyzer = DellAnalyzer.getInstance()
-  new Crowller(url, analyzer)
-  res.send('data')
+router.post('/getData', (req: Request, res: Response) => {
+  const { password } = req.body
+  if (password === '123') {
+    const secret = 'x3b174jsx'
+    const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`
+    const analyzer = DellAnalyzer.getInstance()
+    new Crowller(url, analyzer)
+    res.send('success')
+  } else {
+    res.send('password error')
+  }
 })
 
 export default router
