cf2031788870238f42f6f1fd5d1c1f4505764bed
usamihal <usamihal001@163.com>
2023-05-18 22:46:28 +0800

    feat: 多种请求方法装饰器的生成

--- src/router.ts
+++ src/router.ts
@@ -20,21 +20,6 @@
 
 // const router = Router();
 
-// router.post('/login', (req: BodyRequest, res: Response) => {
-//   const { password } = req.body;
-//   const isLogin = req.session ? req.session.login : false;
-//   if (isLogin) {
-//     res.json(getResponseData(false, '已经登陆过'));
-//   } else {
-//     if (password === '123' && req.session) {
-//       req.session.login = true;
-//       res.json(getResponseData(true));
-//     } else {
-//       res.json(getResponseData(false, '登陆失败'));
-//     }
-//   }
-// });
-
 // router.get('/getData', checkLogin, (req: BodyRequest, res: Response) => {
 //   const secret = 'secretKey';
 //   const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`;
